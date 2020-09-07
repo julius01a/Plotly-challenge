9c5ac1fd1fbc9f0725f79164c37423ce9216ce9d
julius01a <66196006+julius01a@users.noreply.github.com>
2020-09-07 23:13:16 +1000

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,10 @@
 // Create a horizontal bar chart using sample_values, otu_ids, otu_labels
-// Read in data from sample.json
-function getData(id) {
-d3.json("samples.json").then((incomingData) => {
+// access data from sample.json
+function getSample(id) {
+d3.json("samples.json").then((Data) => {
     // console.log(incomingData);
 
-    var samplesData = incomingData.samples;
+    var samplesData = Data.samples;
     // console.log(samplesData);
 
     // Filter for specified id
@@ -93,3 +93,97 @@ function indivInfo(id) {
         });
     });
 }
+
+// Adapt gauge chart to plot weekly washing frequency of individual
+function gaugeInfo(id) {
+    d3.json("samples.json").then((incomingData) => {
+        // console.log(incomingData);
+    
+        var metadata = incomingData.metadata;
+        // console.log(metadata);
+    
+        // Filter for specified id
+        var metaResult = metadata.filter(row => row.id.toString() === id)[0];
+    
+        // Identify frequency of washing
+        var wfreqData = metaResult.wfreq;
+
+        // Modify gauge to account for values ranging from 0 to 9
+        var data = [
+            {
+              type: "indicator",
+              mode: "gauge+number",
+              value: wfreqData,
+              title: { text: "<b>Belly Button Washing Frequency</b> <br> Washing(s) per Week </br>", font: { size: 16 } },
+              gauge: {
+                axis: { range: [null, 9], tickwidth: 1, tickcolor: "black" },
+                bgcolor: "white",
+                bar: { color: "#008080" },
+                borderwidth: 2,
+                bordercolor: "gray",
+                steps: [
+                  { range: [0, 1], text: "0-1", textinfo: 'text', textposition: 'inside', color: "#FFFFE0"},
+                  { range: [1, 2], text: "1-2", color: "#FFFACD" },
+                  { range: [2, 3], text: "2-3", color: "#FAFAD2" },
+                  { range: [3, 4], text: "3-4", color: "#FFEFD5" },
+                  { range: [4, 5], text: "4-5", color: "#FFE4B5" },
+                  { range: [5, 6], text: "5-6", color: "#FFDAB9" },
+                  { range: [6, 7], text: "6-7", color: "#EEE8AA" },
+                  { range: [7, 8], text: "7-8", color: "#F0E68C" },
+                  { range: [8, 9], text: "8-9", color: "#BDB76B" },
+                ],
+              }
+            }
+          ];
+          
+          var layout = {
+            width: 350,
+            height: 300,
+            margin: { t: 25, r: 25, l: 25, b: 25 },
+            font: { color: "black", family: "Arial" },
+            annotation: [{
+              text: "Washes per Week",
+                font: {
+                size: 10,
+                color: 'black'},
+              }],
+          };
+          
+        // Create plot
+          Plotly.newPlot('gauge', data, layout);
+        });
+}
+
+// Create function for default data
+function init() {
+
+    // Link to drop down menu
+    var dropDownMenu = d3.select("#selDataset");
+    // read the data 
+    d3.json("samples.json").then((data)=> {
+        // console.log(data)
+
+        // Populate ID data into drop down menu
+        data.names.forEach(function(name) {
+            dropDownMenu.append("option").text(name).property("value");
+        });
+
+        // call the functions to display the data and the plots to the page
+        var id = d3.select("#selDataset").node().value;
+        console.log(id);
+      
+        // Build the plot with the new stock
+        getSample(id);
+        indivInfo(id);
+        gaugeInfo(id);
+    });
+}
+
+// Create change event trigger
+function optionChanged(id) {
+    getSample(id);
+    indivInfo(id);
+    gaugeInfo(id);
+}
+
+init();
